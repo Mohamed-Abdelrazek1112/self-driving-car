2cdcd6bef420aeb33b633bf33e4e190c691959bf
Mohamed-Abdelrazek1112 <57236689+Mohamed-Abdelrazek1112@users.noreply.github.com>
2022-10-02 21:54:35 +0100

    Fix car.js

--- car.js
+++ car.js
@@ -14,22 +14,22 @@ class Car{
     }
 
     update(){
-        if(this.controls.forward){
-            this.speed+=this.accelration;
-        }
+        this.#move();
+    }
 
-        if(this.controls.reverse){
-            this.speed-=this.accelration;
-        }
-
+    #move(){
+        this.controls.forward?this.speed+=this.accelration:null;
+        this.controls.reverse?this.speed-=this.accelration:null;
         this.speed>this.maxSpeed?this.speed=this.maxSpeed:null;
         -this.speed>this.maxSpeed?this.speed=-this.maxSpeed:null;
         this.speed>0?this.speed-=this.friction:null;
         this.speed<0?Math.min(this.speed+=this.friction,0):null;
         Math.abs(this.speed)<this.friction?this.speed=0:null;
-        this.controls.right?this.angle-=0.03:null;
-        this.controls.left?this.angle+=0.03:null;
-        
+        if(this.speed!=0){
+            const flip = this.speed>0?1:-1;
+            this.controls.right?this.angle-=0.03*flip:null;
+            this.controls.left?this.angle+=0.03*flip:null;
+        }
         this.x-=Math.sin(this.angle)*this.speed;
         this.y-=Math.cos(this.angle)*this.speed;
     }
